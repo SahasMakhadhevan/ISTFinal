fe605dc109a365158864b1d9a7eee0aba1018a46
SahasMakhadhevan <code@sahas.dev>
2023-04-26 16:19:44 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import requests
+
+import service
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+TEMPLATES_AUTO_RELOAD = True
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/top-stats')
+def top_stats():
+    movies = service.get_top_stats()
+    return render_template('top-stats.html', movies=movies)
+
+
+@app.route('/transactions')
+def transactions():
+    transaction_list = service.get_transactions()
+    return render_template('transactions.html', transactions=transaction_list)
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    searchq = request.form.get('searchMovie')
+    movielist = service.search(searchq)
+    return render_template('search.html', movies=movielist)
+
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+    moviedict = service.get_movie(movie_id)
+    return render_template('movie.html', movie=moviedict)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ service.py
@@ -0,0 +1,17 @@
+def get_top_stats():
+    movies = []
+    customers = []
+
+    return movies, customers
+
+
+def get_transactions():
+    return []
+
+
+def search(searchq):
+    return []
+
+
+def get_movie(movie_id):
+    return []
